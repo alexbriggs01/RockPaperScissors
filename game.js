d246d9d7c91c3360bf745772907bfd61ef955f3c
Alex Briggs <alex_briggs01@yahoo.com>
2025-01-13 16:02:15 -0600

    Removed the Boolean check for a player victory from the if else structure

--- game.js
+++ game.js
@@ -34,15 +34,17 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toUpperCase();
 
+    playerWins = ( (humanChoice === "ROCK" && computerChoice === "SCISSORS") 
+                  || (humanChoice === "SCISSORS" && computerChoice === "PAPER") 
+                  || (humanChoice === "PAPER" && computerChoice === "ROCK")
+                 );
+
     if(humanChoice === computerChoice) {
         console.log(`${humanChoice} ties with ${computerChoice}`);
     }
-    else if( (humanChoice === "ROCK" && computerChoice === "SCISSORS") 
-            || (humanChoice === "SCISSORS" && computerChoice === "PAPER") 
-            || (humanChoice === "PAPER" && computerChoice === "ROCK") 
-        ) {
-           console.log(`${humanChoice} beats ${computerChoice}`);
-           humanScore++;  
+    else if(playerWins) {
+        console.log(`${humanChoice} beats ${computerChoice}`);
+        humanScore++;  
     }
     else {
         console.log(`${humanChoice} loses to ${computerChoice}`);
